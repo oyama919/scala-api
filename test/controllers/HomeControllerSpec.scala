395200e4ab8c1c7ad874519bb18decd36bee58f6
oyama919 <oyama919@gmail.com>
2018-10-29 23:36:16 +0900

    add result type to Test/HomeController

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -6,10 +6,13 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.Result
 import play.api.test._
 import play.api.test.Helpers._
 import scalikejdbc.PlayModule
 import services.{MockUserService, UserService}
+
+import scala.concurrent.Future
 
 class HomeControllerSpec extends FunSpec
   with MustMatchers
@@ -27,7 +30,7 @@ class HomeControllerSpec extends FunSpec
     describe("HomeController") {
       describe("route of HomeController#index") {
         it("should be valid") {
-          val result = route(app, FakeRequest(GET, routes.HomeController.index().toString)).get
+          val result: Future[Result] = route(app, FakeRequest(GET, routes.HomeController.index().toString)).get
           status(result) mustBe OK
         }
         it("should be valid when logged in") {
