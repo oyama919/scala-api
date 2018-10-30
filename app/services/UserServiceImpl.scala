48a8aa7a1335255be8774d090e15adccae29b5ab
oyama919 <oyama919@gmail.com>
2018-10-31 00:09:46 +0900

    add comment

--- app/services/UserServiceImpl.scala
+++ app/services/UserServiceImpl.scala
@@ -10,7 +10,11 @@ import scala.util.Try
 @Singleton
 class UserServiceImpl extends UserService {
 
-  // ユーザーの作成に成功した場合は、Success(AUTO_INCREMENTによるID値を返します)
+  /** scala.util.Try
+    *   success: scala.util.Success
+    *   error: scala.util.Failure
+    */
+  // success: Long AUTO_INCREMENTによるID値  error: Failure
   def create(user: User)(implicit dbSession: DBSession = AutoSession): Try[Long] = Try {
     User.create(user)
   }
